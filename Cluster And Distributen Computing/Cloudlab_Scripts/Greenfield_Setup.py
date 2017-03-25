7c30e061e27dcfac97be75d731726a33dd8dbdc1
BuildTools <unconfigured@null.spigotmc.org>
2017-03-25 03:16:40 -0400

    change ubuntu to centos

--- Cluster And Distributen Computing/Cloudlab_Scripts/Greenfield_Setup.py
+++ Cluster And Distributen Computing/Cloudlab_Scripts/Greenfield_Setup.py
@@ -6,30 +6,30 @@ pc = portal.Context()
 request = rspec.Request()
 
 pc.defineParameter("workerCount",
-                   "Number of Greenfield slaves",
-                   portal.ParameterType.INTEGER, 3)
+					"Number of Greenfield slaves",
+					portal.ParameterType.INTEGER, 3)
 
 pc.defineParameter("controllerHost", "Name of controller node",
-                   portal.ParameterType.STRING, "node0", advanced=True,
-                   longDescription="The short name of the controller node.  You shold leave this alone unless you really want the hostname to change.")
-				  
+					portal.ParameterType.STRING, "controllerHost", advanced=True,
+					longDescription="The short name of the controller node.	You shold leave this alone unless you really want the hostname to change.")
+					
 pc.defineParameter("database", "Name of controller node",
-                   portal.ParameterType.STRING, "database", advanced=True,
-                   longDescription="The short name of the controller node.  You shold leave this alone unless you really want the hostname to change.")
-				   
+					portal.ParameterType.STRING, "database", advanced=True,
+					longDescription="The short name of the controller node.	You shold leave this alone unless you really want the hostname to change.")
+					
 params = pc.bindParameters()
 
 tourDescription = "This profile provides a configurable HPCCSystems testbed one master and customizable thorslaves (multiples of four)."
 
 tourInstructions = \
-  """
+	"""
 ### Basic Instructions
-Once your experiment nodes have booted, and this profile's configuration scripts have finished deploying HPCCSystems inside your experiment, you'll be able to visit [the ECLWatch interface](http://{host-%s}:8010) (approx. 5-15 minutes).  
+Once your experiment nodes have booted, and this profile's configuration scripts have finished deploying HPCCSystems inside your experiment, you'll be able to visit [the ECLWatch interface](http://{host-%s}:8010) (approx. 5-15 minutes).	
 """ % (params.controllerHost)
 
 #
 # Setup the Tour info with the above description and instructions.
-#  
+#	
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
@@ -42,73 +42,43 @@ request.addTour(tour)
 # Create a link with type LAN
 link = request.LAN("lan")
 
-# Generate database 
-node = request.RawPC("database")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"
-iface = node.addInterface("if" + "5")
-iface.component_id = "eth1"
-iface.addAddress(rspec.IPv4Address("192.168.1.5", "255.255.255.0"))
-link.addInterface(iface)
-    
-node.addService(rspec.Execute(shell="/bin/sh",
-		command="sudo adduser --ingroup admin --disabled-password hpcc"))
-
-node.addService(rspec.Execute(shell="/bin/sh", 
-		command="sudo adduser hpcc sudo"))
-
-node.addService(rspec.Execute(shell="/bin/sh", 
-		command="sudo apt-get update"))
-
-node.addService(rspec.Execute(shell="/bin/sh",
-        command="sudo wget http://cdn.hpccsystems.com/releases/CE-Candidate-5.2.2/bin/platform/hpccsystems-platform-community_5.2.2-1trusty_amd64.deb"))
-
-node.addService(rspec.Execute(shell="/bin/sh",
-        command="sudo dpkg -i hpccsystems-platform-community_5.2.2-1trusty_amd64.deb"))
-
-node.addService(rspec.Execute(shell="/bin/sh",
-        command="sudo apt-get -y -f install"))
-
-getEnvFile = "sudo wget https://raw.githubusercontent.com/clemsonbds/hpccsystems/master/environments/" + str(params.workerCount) + ".xml -O /etc/HPCCSystems/environment.xml"
-
-node.addService(rspec.Execute(shell="/bin/sh",
-        command=getEnvFile))
-
-node.addService(rspec.Execute(shell="/bin/sh",
-        command="sudo wget https://raw.githubusercontent.com/clemsonbds/hpccsystems/master/conf/environment.conf -O /etc/HPCCSystems/environment.conf"))
-
-
+# Generate the nodes
+for i in range(params.workerCount + 2):
+	if i == 0:
+		node = request.RawPC("controllerHost")
+	elif i == 1:
+		node = request.RawPC("database")
+	else:
+		node = request.RawPC("node" + str(i))
 		
-# Generate the slave nodes
-for i in range(params.workerCount + 1):
-    node = request.RawPC("node" + str(i))
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(rspec.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
-    
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo adduser --ingroup admin --disabled-password hpcc"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo adduser hpcc sudo"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo apt-get update"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo wget http://cdn.hpccsystems.com/releases/CE-Candidate-5.2.2/bin/platform/hpccsystems-platform-community_5.2.2-1trusty_amd64.deb"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo dpkg -i hpccsystems-platform-community_5.2.2-1trusty_amd64.deb"))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo apt-get -y -f install"))
-    getEnvFile = "sudo wget https://raw.githubusercontent.com/clemsonbds/hpccsystems/master/environments/" + str(params.workerCount) + ".xml -O /etc/HPCCSystems/environment.xml"
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command=getEnvFile))
-    node.addService(rspec.Execute(shell="/bin/sh",
-                                  command="sudo wget https://raw.githubusercontent.com/clemsonbds/hpccsystems/master/conf/environment.conf -O /etc/HPCCSystems/environment.conf"))
-    if i == 0:
-        node.addService(rspec.Execute(shell="/bin/sh",
-                                      command="sleep 60"))
-        node.addService(rspec.Execute(shell="/bin/sh",
-                                      command="sudo service hpcc-init start"))
+	node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS66-64-STD"
+	iface = node.addInterface("if" + str(i))
+	iface.component_id = "eth1"
+	iface.addAddress(rspec.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
+	link.addInterface(iface)
+	
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo adduser --ingroup admin --disabled-password hpcc"))
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo adduser hpcc sudo"))
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo apt-get update"))
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo wget http://cdn.hpccsystems.com/releases/CE-Candidate-5.2.2/bin/platform/hpccsystems-platform-community_5.2.2-1trusty_amd64.deb"))
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo dpkg -i hpccsystems-platform-community_5.2.2-1trusty_amd64.deb"))
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo apt-get -y -f install"))
+	getEnvFile = "sudo wget https://raw.githubusercontent.com/clemsonbds/hpccsystems/master/environments/" + str(params.workerCount) + ".xml -O /etc/HPCCSystems/environment.xml"
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command=getEnvFile))
+	node.addService(rspec.Execute(shell="/bin/sh",
+									command="sudo wget https://raw.githubusercontent.com/clemsonbds/hpccsystems/master/conf/environment.conf -O /etc/HPCCSystems/environment.conf"))
+	if i == 0:
+		node.addService(rspec.Execute(shell="/bin/sh",
+										command="sleep 60"))
+		node.addService(rspec.Execute(shell="/bin/sh",
+										command="sudo service hpcc-init start"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec(request)
