c25fe01e3057f0d865508dcbda0f39168ec34ba9
BuildTools <unconfigured@null.spigotmc.org>
2017-03-29 01:23:37 -0400

    torque and nfs work started working on ssh

--- Cloudlab_Scripts/keyless_ssh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ssh-keygen -t rsa -P "" -f "~/.ssh"
-cd ~/.ssh
-cp id_rsa.pub authorized_keys
--- Cloudlab_Scripts/setup_nfs_clients.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo apt-get install -y nfs-common
-sudo mkdir -p /nfs/home
-sudo mount 192.168.1.2:/home /nfs/home
