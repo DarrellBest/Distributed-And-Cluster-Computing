0d0b7fc47059e4805c469c5e7d7695bec9fd1bc0
ehoneyc18 <ehoneyc@g.clemson.edu>
2017-04-12 14:34:27 -0400

    Update asg5_reducer3.py

--- asg5/asg5_reducer3.py
+++ asg5/asg5_reducer3.py
@@ -3,30 +3,33 @@ import sys
 from operator import itemgetter
 
 current_task = None
-total_task_count = 0
+current_index = None
+task = None
 
 task_list = []
 
 for line in sys.stdin:
     line = line.strip()
-    task, count = line.split("\t", 1)
+    task, index = line.split("\t", 1)
     try:
-        count = int(count)
+        index = int(index)
     except ValueError:
         continue
     
     if current_task == task:
-        total_task_count += 1
+        if int(index) > int(current_index):
+            current_index = index
     else:
         if current_task:
-            task_list.append((current_task, total_task_count))
+            task_list.append((current_task, current_index))
         current_task = task
-        total_task_count = 1
+        current_index = index
         
 if current_task == task:
-    task_list.append((current_task, total_task_count))
+    task_list.append((current_task, current_index))
     
 task_list = sorted(task_list, key=itemgetter(1), reverse=True)
 
 for x in range(20):
     print("%s\t%s" %(task_list[x][0], task_list[x][1]))
+
